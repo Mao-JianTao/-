8cbbd0265a21c3c357e44e4ded40ff61553d1e48
M T <il>
2020-11-30 17:04:19 +0800

    毛建涛11.30

--- src/components/Bot.vue
+++ src/components/Bot.vue
@@ -1,58 +1,61 @@
 <template>
-    <div id="footer">
-        <van-tabbar v-model="active">
-  <van-tabbar-item badge="3">
-    <span>自定义</span>
-    <template #icon="props">
-      <img :src="props.active ? icon.active : icon.inactive" />
-    </template>
-  </van-tabbar-item>
-  <van-tabbar-item icon="search">标签</van-tabbar-item>
-  <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
-</van-tabbar>
-    </div>
-
-    
+  <div id="footer">
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="orders-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="envelop-o">消息</van-tabbar-item>
+      <van-tabbar-item icon="manager-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
 <script>
-        export default{
-            name:"Bot"
-        }
-
-    </script>
+export default {
+  name: "Bot",
+  data() {
+    return {
+      active: 0,
+      icon: {
+        active: "https://img.yzcdn.cn/vant/user-active.png",
+        inactive: "https://img.yzcdn.cn/vant/user-inactive.png",
+      },
+    };
+  },
+};
+</script>
 
     <style scoped>
-        #footer{
-            width: 100%;
-            height: 50p7x;
-            background: white;
-            border-top: 2px solid #ebe8e3;
-            position: fixed;
-            left: 0;
-            bottom: 0;
-        }
-        #footer ul{
-            display: flex;
-            text-align: center;
-            height: 50px;
-            align-items: center;
-        }
-        #footer ul li{
-            flex: 1;
-            height: 40px;
-        }
-        #footer li.active{
-            color: #000000;
-        }
-        #footer li.router-link-active{
-            color: #838383;
-        }
-        #footer ul i{
-            font-size: 20px;
-        }
-        #footer ul p{
-            font-size: 12px;
-            line-height: 0px;
-        }
-    </style>
+#footer {
+  width: 100%;
+  background: white;
+  border-top: 2px solid #ebe8e3;
+  position: fixed;
+  left: 0;
+  z-index: 999;
+  bottom: 0;
+}
+#footer p{
+  font-size: 14px;
+  line-height: 0px;
+}
+#footer ul {
+  display: flex;
+  text-align: center;
+  align-items: center;
+}
+#footer ul li {
+  flex: 1;
+  height: 40px;
+}
+#footer li.active {
+  color: #000000;
+}
+#footer li.router-link-active {
+  color: #838383;
+}
+#footer ul i {
+  font-size: 20px;
+}
+#footer ul p {
+  font-size: 10px;
+}
+</style>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,24 +2,246 @@
   <div id="app">
     <Top></Top>
     <Bot></Bot>
+    <div id="cent">
+      <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+        <van-swipe-item>
+          <img src="../assets/lun1.png" width="100%" height="150px" alt="" />
+        </van-swipe-item>
+        <van-swipe-item
+          ><img src="../assets/lun2.png" width="100%" height="150px" alt=""
+        /></van-swipe-item>
+        <van-swipe-item
+          ><img src="../assets/lun3.png" width="100%" height="150px" alt=""
+        /></van-swipe-item>
+        <van-swipe-item
+          ><img src="../assets/lun4.png" width="100%" height="150px" alt=""
+        /></van-swipe-item>
+        <van-swipe-item
+          ><img src="../assets/lun5.png" width="100%" height="150px" alt=""
+        /></van-swipe-item>
+        <van-swipe-item
+          ><img src="../assets/lun6.png" width="100%" height="150px" alt=""
+        /></van-swipe-item>
+      </van-swipe>
+    </div>
+    <div id="cent1">
+      <van-grid :border="false" :column-num="5">
+        <van-grid-item>
+          <p><img src="../assets/cent1.png" width="50%" /></p>
+          <p>安全教育</p>
+        </van-grid-item>
+        <van-grid-item>
+          <p><img src="../assets/cent2.png" width="50%" /></p>
+          <p>安全提醒</p>
+        </van-grid-item>
+        <van-grid-item>
+          <p><img src="../assets/cent3.png" width="50%" /></p>
+          <p>安心陪伴</p>
+        </van-grid-item>
+        <van-grid-item>
+          <p><img src="../assets/cent4.png" width="50%" /></p>
+          <p>话题</p>
+        </van-grid-item>
+        <van-grid-item>
+          <p><img src="../assets/cent5.png" width="50%" /></p>
+          <p>热电资讯</p>
+        </van-grid-item>
+      </van-grid>
+    </div>
+
+    <div id="cent2">
+      <van-tabs v-model="active" swipeable animated>
+        <van-tab title="热点" name="a">
+          <div class="cent2-1">
+            <div class="cent2-text">
+             <p> 43车连撞，已致3死六伤！冬天一定要小心这个“杀手”</p>
+             <p>安全资讯 <span class="an">安全</span> </p>
+            </div>
+            <div class="cent2-img"> <img src="../assets/cent2-1.png" width="90%" alt=""> </div>
+          </div>
+          <div class="cent2-1">
+            <div class="cent2-text">
+             <p>老百姓如何做好冬季新冠肺炎疫情防护 </p>
+             <p>安全资讯 <span class="an">安全</span> </p>
+            </div>
+            <div class="cent2-img"> <img src="../assets/cent2-2.png" width="90%" alt=""> </div>
+          </div>
+          <div class="cent2-1">
+            <div class="cent2-text">
+             <p> 43车连撞，已致3死六伤！冬天一定要小心这个“杀手”</p>
+             <p>安全资讯 <span class="an">安全</span> </p>
+            </div>
+            <div class="cent2-img"> <img src="../assets/cent2-1.png" width="90%" alt=""> </div>
+          </div>
+          <div class="cent2-1">
+            <div class="cent2-text">
+             <p> 老百姓如何做好冬季新冠肺炎疫情防护</p>
+             <p>安全资讯 <span class="an">安全</span> </p>
+            </div>
+            <div class="cent2-img"> <img src="../assets/cent2-2.png" width="90%" alt=""> </div>
+          </div>
+         
+        </van-tab>
+        <van-tab title="安全健康" name="b">
+          <div class="cent2-1">
+            <div class="cent2-text">
+             <p>老百姓如何做好冬季新冠肺炎疫情防护 </p>
+             <p>安全资讯 <span class="an">安全</span> </p>
+            </div>
+            <div class="cent2-img"> <img src="../assets/cent2-2.png" width="90%" alt=""> </div>
+          </div>
+          <div class="cent2-1">
+            <div class="cent2-text">
+             <p> 43车连撞，已致3死六伤！冬天一定要小心这个“杀手”</p>
+             <p>安全资讯 <span class="an">安全</span> </p>
+            </div>
+            <div class="cent2-img"> <img src="../assets/cent2-1.png" width="90%" alt=""> </div>
+          </div>
+        </van-tab>
+        <van-tab title="亲子" name="c">
+          <div class="cent2-1">
+            <div class="cent2-text">
+             <p> 43车连撞，已致3死六伤！冬天一定要小心这个“杀手”</p>
+             <p>安全资讯 <span class="an">安全</span> </p>
+            </div>
+            <div class="cent2-img"> <img src="../assets/cent2-1.png" width="90%" alt=""> </div>
+          </div>
+          <div class="cent2-1">
+            <div class="cent2-text">
+             <p>老百姓如何做好冬季新冠肺炎疫情防护 </p>
+             <p>安全资讯 <span class="an">安全</span> </p>
+            </div>
+            <div class="cent2-img"> <img src="../assets/cent2-2.png" width="90%" alt=""> </div>
+          </div>
+        </van-tab>
+        <van-tab title="人文" name="d">
+           <div class="cent2-1">
+            <div class="cent2-text">
+             <p>老百姓如何做好冬季新冠肺炎疫情防护 </p>
+             <p>安全资讯 <span class="an">安全</span> </p>
+            </div>
+            <div class="cent2-img"> <img src="../assets/cent2-2.png" width="90%" alt=""> </div>
+          </div>
+          <div class="cent2-1">
+            <div class="cent2-text">
+             <p> 43车连撞，已致3死六伤！冬天一定要小心这个“杀手”</p>
+             <p>安全资讯 <span class="an">安全</span> </p>
+            </div>
+            <div class="cent2-img"> <img src="../assets/cent2-1.png" width="90%" alt=""> </div>
+          </div>
+        </van-tab>
+        <van-tab title="科学素养" name="e">
+          <div class="cent2-1">
+            <div class="cent2-text">
+             <p> 43车连撞，已致3死六伤！冬天一定要小心这个“杀手”</p>
+             <p>安全资讯 <span class="an">安全</span> </p>
+            </div>
+            <div class="cent2-img"> <img src="../assets/cent2-1.png" width="90%" alt=""> </div>
+          </div>
+          <div class="cent2-1">
+            <div class="cent2-text">
+             <p>老百姓如何做好冬季新冠肺炎疫情防护 </p>
+             <p>安全资讯 <span class="an">安全</span> </p>
+            </div>
+            <div class="cent2-img"> <img src="../assets/cent2-2.png" width="90%" alt=""> </div>
+          </div>
+        </van-tab>
+        <van-tab title="学习力" name="f">
+          <div class="cent2-1">
+            <div class="cent2-text">
+             <p>老百姓如何做好冬季新冠肺炎疫情防护 </p>
+             <p>安全资讯 <span class="an">安全</span> </p>
+            </div>
+            <div class="cent2-img"> <img src="../assets/cent2-2.png" width="90%" alt=""> </div>
+          </div>
+          <div class="cent2-1">
+            <div class="cent2-text">
+             <p> 43车连撞，已致3死六伤！冬天一定要小心这个“杀手”</p>
+             <p>安全资讯 <span class="an">安全</span> </p>
+            </div>
+            <div class="cent2-img"> <img src="../assets/cent2-1.png" width="90%" alt=""> </div>
+          </div></van-tab>
+        <van-tab title="艺术" name="g">
+          <div class="cent2-1">
+            <div class="cent2-text">
+             <p> 43车连撞，已致3死六伤！冬天一定要小心这个“杀手”</p>
+             <p>安全资讯 <span class="an">安全</span> </p>
+            </div>
+            <div class="cent2-img"> <img src="../assets/cent2-1.png" width="90%" alt=""> </div>
+          </div>
+          <div class="cent2-1">
+            <div class="cent2-text">
+             <p>老百姓如何做好冬季新冠肺炎疫情防护 </p>
+             <p>安全资讯 <span class="an">安全</span> </p>
+            </div>
+            <div class="cent2-img"> <img src="../assets/cent2-2.png" width="90%" alt=""> </div>
+          </div> 
+        </van-tab>
+      </van-tabs>
+    </div>
   </div>
 </template>
 
 <script>
-import Top from '../components/Top'
-import Bot from '../components/Bot'
+import Top from "../components/Top";
+import Bot from "../components/Bot";
 export default {
   name: "home",
-  components:{
+  components: {
     Top,
-    Bot
-  }
+    Bot,
+  },
 };
 </script>
 <style scoped>
-*{
+* {
   margin: 0;
   padding: 0;
 }
+.my-swipe .van-swipe-item {
+  line-height: 20px;
+  text-align: center;
+}
 
+#cent1 p {
+  font-size: 14px;
+  text-align: center;
+}
+
+.cent2-1{
+  border-bottom: 1px solid rgb(196, 196, 196);
+  height: 85px;
+}
+.cent2-text{
+  float: left;
+  width: 200px;
+  margin-left: 10px;
+  margin-top: 10px;
+}
+.cent2-text p:nth-of-type(1){
+   font-weight: bold;
+  font-size: 15px;
+}
+.cent2-text p:nth-of-type(2){
+  font-size: 12px;
+  color: rgb(170, 170, 170);
+  margin-top: 10px;
+}
+.an{
+  margin-left: 10px;
+  border-radius:2px ;
+  padding: 0px 5px;
+  color: rgb(39, 42, 247);
+  border: 1px solid rgb(39, 42, 247);
+}
+.cent2-img{
+  width: 120px;
+  float: right;
+  margin-left: 30px;
+  margin-top: 2px;
+  margin-right: 10px;
+}
+.cent2-img img{
+  float: right;
+}
 </style>
